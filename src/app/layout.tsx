eb3e429ce7a6a65d5bca3503947cc22df634d159
DJL - Jack <71508487+JackatDJL@users.noreply.github.com>
2024-12-15 23:06:12 +0100

    Added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from ">/ui/sonner";
 import "./globals.css";
-import {} from ">/Theme";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
